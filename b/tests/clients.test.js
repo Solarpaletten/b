11e88540a8faf37027584bf20f22144c45fa7d5b
Solarpaletten <solarpaletten@gmail.com>
2025-02-09 18:29:21 +0200

    +src app test backend

--- b/tests/clients.test.js
+++ b/tests/clients.test.js
@@ -1,5 +1,5 @@
-const request = require('../src/app');
-const app = require('../src/index');
+const request = require('supertest');
+const app = require('../src/app');
 const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
 
